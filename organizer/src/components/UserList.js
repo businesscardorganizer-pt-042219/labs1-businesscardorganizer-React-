1e5ea8ca6978c3d8fc4f1a4aae631198d641d3b4
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-01 16:43:55 -0700

    Added basic component for creating collections tags

--- organizer/src/components/UserList.js
+++ organizer/src/components/UserList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCards, deleteCard} from '../actions';
@@ -7,6 +6,7 @@ import { getCards, deleteCard} from '../actions';
 
 import User from "./User";
 import Navigation from "./Navigation";
+import CollectionTabs from "./CollectionTabs";
 
 import SpinnerDataLoad from "./SpinnerDataLoad";
 
@@ -31,7 +31,11 @@ class UserList extends React.Component {
     return (
       <div className="user-list">
         <Navigation />
+        <CollectionTabs />
         <div className="active-user-list">
+          {/* Future tab component */}
+          {/* <-------- Future tab component */}
+
           { this.props.fetchingCards ? <SpinnerDataLoad /> :
           (this.props.cards
             .filter(card => {
