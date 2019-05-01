a0b5b77568196dc47b4b03d2cdd0fe8e3261ee74
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-01 12:01:16 -0700

    Made user component which generate the single card view display only fields that have info in DB for them and hide empty fields

--- organizer/src/components/User.js
+++ organizer/src/components/User.js
@@ -28,29 +28,42 @@ function User(props) {
         
         <div className="card-body">
           <h4>Contact information</h4>
-          <div className="card-body-item">
-            <img className="phone-icon" src={PhoneIcon} alt="" />
-            <div>{props.card.cell_phone}</div>
-          </div>
-          <div className="card-body-item">
-            <img className="email-icon" src={EmailIcon} alt="" />
-            <div>{props.card.email}</div>
-          </div>
-          <div className="card-body-item">
-            <img className="linkedin-icon" src={LinkedinIcon} alt="" />
-            <a href={props.card.linkedIn} alt="linkedin">LinkedIn</a>
-          </div>
-          <div className="card-body-item">
-            <img className="github-icon" src={GithubIcon} alt="" />
-            <a href={props.card.github} alt="github">GitHub</a>
-          </div>
+          {/* Making checks for every item if we have info in DB, if not, we're just not showing this line */}
+          {
+            props.card.cell_phone && (
+              <div className="card-body-item">
+                <img className="phone-icon" src={PhoneIcon} alt="" />
+                <div>{props.card.cell_phone}</div>
+              </div>
+            )
+          }
+          {
+            props.card.email && (
+              <div className="card-body-item">
+                <img className="email-icon" src={EmailIcon} alt="" />
+                <div>{props.card.email}</div>
+              </div>
+            )
+          }
+          {
+            props.card.linkedIn && (
+              <div className="card-body-item">
+                <img className="linkedin-icon" src={LinkedinIcon} alt="" />
+                <a href={props.card.linkedIn} alt="linkedin">LinkedIn</a>
+              </div>
+            )
+          }
+          {
+            props.card.github && (
+              <div className="card-body-item">
+                <img className="github-icon" src={GithubIcon} alt="" />
+                <a href={props.card.github} alt="github">GitHub</a>
+              </div>
+            )
+          }
         </div>
 
       </div>
-        
-      {/* <div className="user-img">
-        <img src={props.card.avatar} alt="user-avatar" />
-      </div> */}
 
       <div className="user-controls">
         <p className="user-edit" /* Add link to the edit form page here */>✎</p>
